778dd79ed303698efeb9773052c5c7c02686cf79
Joana <joanajanefdez@gmail.com>
2022-08-18 11:01:09 +0200

    prueba 3 -añadir bean

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -1,16 +1,41 @@
 package com.ud26_SpringMySQL_Ex1;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@EnableWebSecurity
+@SuppressWarnings("deprecation")
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfiguration{
-	
-	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated()
-		.and().formLogin().permitAll().and().httpBasic();
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.permitAll()
+				.and()
+				.httpBasic();
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("jone")
+				.password("1234")
+				.roles("admin")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 }
