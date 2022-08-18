5df961189eb6890e1277fa5838529fe5d69c35bb
Joana <joanajanefdez@gmail.com>
2022-08-18 12:02:00 +0200

    fixing2

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig implements WebMvcConfigurer  {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/").allowedMethods("*");
+    	registry.addMapping("*").allowedOrigins("*").allowedMethods("*");
     }
 
 }
