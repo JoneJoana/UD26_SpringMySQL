94fe1130edad2c1508ae292d4693307b50b2c988
Joana <joanajanefdez@gmail.com>
2022-08-18 10:31:12 +0200

    quitar update, actualizar data.sql(testimonial)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-#testimonial - se gestionan desde el mismo workbench (ahora en local) -- ver que pasa al subirlo a Heroku
+#testimonial - se gestionan desde el mismo workbench/postman
 INSERT INTO `piezas`(nombre) VALUES('tornillo'),('mango'),('espina'),('marco');
 INSERT INTO `Proveedores` VALUES ('p1','Jose'),('p2','Oliver'),('p3','Joan'),('p4','Toni');
-INSERT INTO `suministra`(precio,pieza,proveedor) VALUES (4,1,'p1'),(3,2,'p1'),(3,4,'p2'),(2,5,'p1'),(1,3,'p3'),(20,1,'p3');
+INSERT INTO `suministra`(precio,pieza,proveedor) VALUES (4,1,'p1'),(3,11,'p1'),(3,31,'p2'),(2,21,'p1'),(1,1,'p3'),(20,31,'p3');
+#los indices de piezas van de 10 en 10
