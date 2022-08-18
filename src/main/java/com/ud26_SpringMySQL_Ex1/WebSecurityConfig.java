a38e7a1115c83394064ba798624722956a7dfcdc
Joana <joanajanefdez@gmail.com>
2022-08-18 13:45:04 +0200

    quitar deprecated

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -11,10 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer  {
+public class WebSecurityConfig  implements WebMvcConfigurer  {
 
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception {
     	http
     	.csrf().disable()
     	.authorizeRequests()
