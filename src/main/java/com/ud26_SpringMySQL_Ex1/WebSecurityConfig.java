7b9d6d55f1849ba9e4c6b4594ab28e59e7e6051c
Joana <joanajanefdez@gmail.com>
2022-08-18 11:58:12 +0200

    fixing

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -4,15 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer  {
+public class WebSecurityConfig implements WebMvcConfigurer  {
 
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
         .csrf().disable()
