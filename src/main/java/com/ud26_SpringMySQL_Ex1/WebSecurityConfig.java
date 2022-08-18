aa5f130594d79985041211d0aa9aaf372ff64bed
Joana <joanajanefdez@gmail.com>
2022-08-18 11:40:38 +0200

    webSecurity concreto

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -12,11 +12,11 @@ public class WebSecurityConfig{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
         .authorizeRequests()
-        .antMatchers(HttpMethod.OPTIONS,"/**").authenticated()
-        .antMatchers(HttpMethod.GET,"/**").authenticated()
-        .antMatchers(HttpMethod.POST,"/**").authenticated()
-        .antMatchers(HttpMethod.PUT,"/**").authenticated()
-        .antMatchers(HttpMethod.DELETE,"/**").hasRole("admin")
+        .antMatchers(HttpMethod.GET,"/api/piezas").authenticated()
+        .antMatchers(HttpMethod.GET,"/api/piezas/**").authenticated()
+        .antMatchers(HttpMethod.POST,"/api/piezas").authenticated()
+        .antMatchers(HttpMethod.PUT,"/api/piezas/**").authenticated()
+        .antMatchers(HttpMethod.DELETE,"/api/piezas/**").hasRole("admin")
         .anyRequest().authenticated()
         .and().formLogin().permitAll()
         .and().httpBasic();
