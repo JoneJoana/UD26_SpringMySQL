1c35b6116f02f5cf1187e8f269c7fdf8ccc9b987
Joana <joanajanefdez@gmail.com>
2022-08-18 10:38:42 +0200

    extends WebSecurityConfiguration

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -3,10 +3,11 @@ package com.ud26_SpringMySQL_Ex1;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig{
+public class WebSecurityConfig extends WebSecurityConfiguration{
 	
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated()
