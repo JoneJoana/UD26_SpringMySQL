e2627758199af730cd25ea504d3485c25201ff73
Joana <joanajanefdez@gmail.com>
2022-08-18 10:45:24 +0200

    change SecurityFilterChain

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -1,16 +1,22 @@
 package com.ud26_SpringMySQL_Ex1;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfiguration{
+public class WebSecurityConfig{
 	
-	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated()
-		.and().formLogin().permitAll().and().httpBasic();
-	}
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic();
+        return http.build();
+    }
 }
