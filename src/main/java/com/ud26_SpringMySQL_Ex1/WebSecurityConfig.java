f08f6d27c96bcb756e8aacdae96c7c802b490cc6
Joana <joanajanefdez@gmail.com>
2022-08-18 10:51:22 +0200

    reset webSecurity

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -1,22 +1,16 @@
 package com.ud26_SpringMySQL_Ex1;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig{
+public class WebSecurityConfig extends WebSecurityConfiguration{
 	
-	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-            )
-            .httpBasic();
-        return http.build();
-    }
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated()
+		.and().formLogin().permitAll().and().httpBasic();
+	}
 }
