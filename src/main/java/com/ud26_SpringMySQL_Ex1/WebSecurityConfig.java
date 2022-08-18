9c6ff1e01874d48f8d74539541fd9f4ac4ec6320
Joana <joanajanefdez@gmail.com>
2022-08-18 11:55:07 +0200

    retoque

--- src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/WebSecurityConfig.java
@@ -4,21 +4,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebSecurity
 @Configuration
-public class WebSecurityConfig{
-	
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer  {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .csrf().disable()
         .authorizeRequests()
-        .antMatchers(HttpMethod.GET,"/api/piezas").authenticated()
-        .antMatchers(HttpMethod.GET,"/api/piezas/**").authenticated()
-        .antMatchers(HttpMethod.POST,"/api/piezas").authenticated()
-        .antMatchers(HttpMethod.PUT,"/api/piezas/**").authenticated()
-        .antMatchers(HttpMethod.DELETE,"/api/piezas/**").hasRole("admin")
+        .antMatchers(HttpMethod.OPTIONS,"/").authenticated()
+        .antMatchers(HttpMethod.GET,"/").authenticated()
+        .antMatchers(HttpMethod.POST,"/").authenticated()
+        .antMatchers(HttpMethod.PUT, "/").authenticated()
+        .antMatchers(HttpMethod.DELETE,"/").hasRole("admin")
         .anyRequest().authenticated()
         .and().formLogin().permitAll()
         .and().httpBasic();
-	}
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/").allowedMethods("*");
+    }
+
 }
