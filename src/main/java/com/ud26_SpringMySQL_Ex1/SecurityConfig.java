d9251410002acfe54881904c99483f015e9b1c1a
Joana <joanajanefdez@gmail.com>
2022-08-19 15:05:39 +0200

    addCorsMapping

--- src/main/java/com/ud26_SpringMySQL_Ex1/SecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig  implements WebMvcConfigurer  {
 
 	protected void configure(HttpSecurity http) throws Exception {
     	http
@@ -22,6 +24,11 @@ public class SecurityConfig {
         .anyRequest().authenticated()
         .and().formLogin().permitAll()
         .and().httpBasic();
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*").allowedOrigins("*").allowedMethods("*");
     }
 
 }
