20b5a168fa12707ba551a676ca9098b32fad2e4a
Joana <joanajanefdez@gmail.com>
2022-08-21 20:21:17 +0200

    fix securityConfig - segun version spring correcta

--- src/main/java/com/ud26_SpringMySQL_Ex1/SecurityConfig.java
+++ src/main/java/com/ud26_SpringMySQL_Ex1/SecurityConfig.java
@@ -1,34 +1,24 @@
 package com.ud26_SpringMySQL_Ex1;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  implements WebMvcConfigurer  {
+public class SecurityConfig {
 
-	protected void configure(HttpSecurity http) throws Exception {
-    	http
-    	.csrf().disable()
-    	.authorizeRequests()
-    	.antMatchers(HttpMethod.OPTIONS, "/**").authenticated()
-        .antMatchers(HttpMethod.GET,"/**").authenticated()
-        .antMatchers(HttpMethod.POST,"/**").authenticated()
-        .antMatchers(HttpMethod.PUT, "/**").authenticated()
-        .antMatchers(HttpMethod.DELETE,"/**").hasRole("admin")
-        .anyRequest().authenticated()
-        .and().formLogin().permitAll()
-        .and().httpBasic();
-    }
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*").allowedOrigins("*").allowedMethods("*");
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated()
+		.and().httpBasic();
+		
+		return http.build();
+	}
 
 }
